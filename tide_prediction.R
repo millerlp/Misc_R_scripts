dc7efff3011dc7a1578b1cce678814d9ec5eed32
Luke Miller <millerlp@gmail.com>
2013-05-19 12:03:07 -0700

    Adding some more comments and links to original sources

--- tide_prediction.R
+++ tide_prediction.R
@@ -1,16 +1,23 @@
 # Filename: tide_prediction.R
 
-# Generate tide height predictions for NOAA *reference* tide stations. This 
-# requires a previously-produced file of harmonics for the sites, stored as a 
-# Rdata file.
+# NOT FOR NAVIGATIONAL USE. DON'T WRECK YOUR BOAT BASED ON MY BAD CODING.
+# Generate tide height predictions for US NOAA *reference* tide stations 
+# entirely within R.  
+# This requires a previously-produced file of harmonics for the sites, stored as 
+# a Rdata file. The current file is harmonics-20120302.Rdata. The harmonics file 
+# was originally derived from XTide, found at http://www.flaterco.com/xtide/
 # The available reference tide stations are listed here:
 # http://www.flaterco.com/xtide/locations.html
 # Note that only stations listed as type 'Ref' on that page can be used 
 # with this script.
-# Currently the script assumes that all input times (and corresponding tide 
+# The script assumes that all input times (and corresponding tide 
 # time outputs) are in UTC/GMT time zone, not your local time zone. Check the 
 # output against NOAA's tide predictions at 
 # http://tidesandcurrents.noaa.gov/tide_predictions.shtml
+# This can produce predictions for years between 1700 and 2100, but with 
+# decreasing accuracy as you move further from 2012 (say beyond 1990 or 2030)
+# since the harmonics file is derived from the current tidal epoch, and things 
+# like sea level have a tendency to change over time.
 
 # Author: Luke Miller  May 19, 2013
 ###############################################################################
